5a9c005b765a7487f9febc8cae4938fa61b5aea1
Sanket Mohapatra <www.sanket1mohapatra@gmail.com>
2021-04-14 19:46:24 +0530

    Fix

--- Generate_dataset.py
+++ Generate_dataset.py
@@ -25,23 +25,24 @@ elevators = {}
 
 number_of_items = number_of_items_per_class[0]
 temperature = truncated_normal_floats(
-    mean=57.5, sd=10, low=25, upp=90, num=number_of_items)
+    mean=57.5, sd=10, low=25, upp=80, num=number_of_items)\
+
 speed = truncated_normal_ints(mean=1, sd=1, low=0, upp=3, num=number_of_items)
 voltage = truncated_normal_ints(
     mean=230, sd=15, low=100, upp=260, num=number_of_items)
-emergency = np.ones(number_of_items)
-data = np.column_stack((temperature, speed, voltage, emergency))
+# emergency = np.ones(number_of_items)
+data = np.column_stack((temperature, speed, voltage))
 elevators["Healthy"] = data
 # Unhealthy Elevator
 number_of_items = number_of_items_per_class[1]
 
 temperature = truncated_normal_floats(
-    mean=92, sd=10, low=25, upp=90, num=number_of_items)
+    mean=88, sd=13, low=82, upp=90, num=number_of_items)
 speed = truncated_normal_ints(mean=1, sd=1, low=0, upp=3, num=number_of_items)
 voltage = truncated_normal_ints(
     mean=130, sd=15, low=100, upp=260, num=number_of_items)
-emergency = np.ones(number_of_items)
-data = np.column_stack((temperature, speed, voltage, emergency))
+# emergency = np.zeros(number_of_items)
+data = np.column_stack((temperature, speed, voltage))
 
 elevators["Unhealthy"] = data
 
@@ -51,7 +52,7 @@ data = np.concatenate((elevators["Healthy"], elevators["Unhealthy"]), axis=0)
 
 target = np.zeros(sum(number_of_items_per_class))
 previous_end = 0
-for i in range(1, 3):
+for i in range(1, 2):
     num = number_of_items_per_class[i - 1]
     beg = previous_end
     target[beg:beg+num] += i
